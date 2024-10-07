f079ac8796d9c04efd5ba0b43d8d83b214105600
Ilya Merzlikin <imerzlikin@diasoft.ru>
2024-10-07 16:43:02 +0300

    Добавил кнопки в веб-компоненты

--- src/components/dialog/angular-wc/angular-custom-element.component.ts
+++ src/components/dialog/angular-wc/angular-custom-element.component.ts
@@ -2,17 +2,22 @@ import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
     selector: 'app-my-web',
-    template: `<p>Это мой веб-компонент! Привет, {{ name }}!</p>`,
+    template: `
+        <p>Это мой веб-компонент!</p>
+        <button (click)="onClick()">Нажми меня</button>
+    `,
     standalone: true
 })
 export class AngularCustomElement implements OnInit, OnDestroy {
-    @Input() name: string = 'мир';
-
     ngOnInit() {
         console.log('MyWebComponent добавлен в DOM');
     }
 
     ngOnDestroy() {
         console.log('MyWebComponent удален из DOM');
+    }
+
+    onClick() {
+        console.log('Ты нажал меня')
     }
 }
--- src/components/dialog/normal-wc/custom.element.ts
+++ src/components/dialog/normal-wc/custom.element.ts
@@ -3,17 +3,23 @@ export class CustomElement extends HTMLElement {
         super();
         const shadow = this.attachShadow({ mode: 'open' });
         const wrapper = document.createElement('div');
-        wrapper.innerHTML = `<p>Это мой веб-компонент!</p>`;
+        wrapper.innerHTML = `
+      <p>Это мой веб-компонент!</p>
+      <button id="myButton">Нажми меня</button>
+    `;
         shadow.appendChild(wrapper);
+
+        const button = wrapper.querySelector('#myButton');
+        button?.addEventListener('click', () => {
+            console.log('Ты нажал меня CustomElement');
+        });
     }
 
     connectedCallback() {
         console.log('MyWebComponent добавлен в DOM');
-        // Дополнительная логика инициализации
     }
 
     disconnectedCallback() {
         console.log('MyWebComponent удален из DOM');
-        // Дополнительная логика очистки
     }
 }
